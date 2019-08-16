2d4986cdbf9568552df129087ee82331a790da46
jtwray <tuckerwray84@gmail.com>
2019-08-16 19:59:28 -0400

    creates a headerCreator Function()

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,55 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
+const monthNames = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
 
-function Header() {}
+const todayDateMonYr=() => {
+     const today = new Date();
+    let mm = monthNames[today.getMonth()];
+    let dd = today.getDate();
+    let yyyy = today.getFullYear();
+    let dateMonth=`${dd} ${mm} ${yyyy}`;
+    return dateMonth;
+}
+
+function Header() {
+    
+    const headerDiv=document.createElement( "div" );
+    headerDiv.classList.add( "header" );
+    
+    const date=document.createElement( "span" );
+    date.classList.add( "date" );
+    date.textContent=todayDateMonYr();
+    headerDiv.appendChild( date );
+    
+
+    const h1=document.createElement( "h1" );
+    h1.textContent="Lamda Times";
+    headerDiv.appendChild( h1 );
+    
+
+    const temp=document.createElement( "span" );
+    temp.classList.add( "temp" );
+    temp.textContent="98°";
+    headerDiv.appendChild( temp );
+    
+    const headerContainer=document.querySelector( ".header-container" );
+    console.log('correctfile')
+    return headerContainer.appendChild( headerDiv );
+    
+}
+
+Header();
