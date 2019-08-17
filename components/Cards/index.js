599732cbd045dea75d24abd84b35aeae4a324521
jtwray <tuckerwray84@gmail.com>
2019-08-17 18:16:55 -0400

    corrects a typo in createArticleCard(): adds $ to ${article.authoPhoto},moves articleImg and appends to articleImgDiv instead of authorDiv to gain correct class styling, Uses Object.values to map through response.data.articles properties rather than Array.from (object)  working here for me

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -5,7 +5,8 @@
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
-//
+///**big object=response.data */
+
 // <div class="card">
 //   <div class="headline">{Headline of article}</div>
 //   <div class="author">
@@ -18,26 +19,16 @@
 
 // Create a card for each of the articles and add the card to the DOM.
 const createTopic = (arr) => {
-	arr.map((article) => {
-         createArticleCard( article );
-        return cardsContainer.appendChild(articleCard)
+	console.log("arr:", arr);
+	Array.from(arr).map((article) => {
+		return createArticleCard(article);
 	});
 };
-axios
-	.get(`https://lambda-times-backend.herokuapp.com/articles`)
-	.then((response) => {
-		const articles = response.data.articles;
-		console.log('articles:',articles);
-		 articles.map((articleTopic) => {
-             createTopic( articleTopic );
-             
-		});
-	})
-	.catch(function(error) {
-		console.log(error);
-	});
 
-const createArticleCard = (article) => {
+const cardsContainer = document.querySelector(".cards-container");
+console.log("cardsContainer:", cardsContainer);
+
+const createArticleCard = (article = {}) => {
 	const articleCard = document.createElement("card");
 	articleCard.classList.add("card");
 
@@ -55,13 +46,33 @@ const createArticleCard = (article) => {
 	authorDiv.appendChild(articleImgDiv);
 
 	const articleImg = document.createElement("img");
-	articleImg.src = `{article.authorPhoto}`;
-	authorDiv.appendChild(articleImg);
+	articleImg.src = `${article.authorPhoto}`;
+	articleImgDiv.appendChild(articleImg);
 
 	const authorsName = document.createElement("span");
 	authorsName.textContent = `By:${article.authorName}`;
 	authorDiv.appendChild(authorsName);
 	console.log("articleCard:", articleCard);
-       return articleCard;
+	cardsContainer.appendChild(articleCard);
 };
-const cardsContainer=document.querySelector( 'cards-container' );
+const fetchData = () => {
+	axios
+		.get(`https://lambda-times-backend.herokuapp.com/articles`)
+		.then((response) => {
+			const eachArticle = Object.values(response.data.articles).map(
+				(topic) => {
+					return Object.values(topic).map((article) => {
+						return createArticleCard(article);
+					});
+				}
+			);
+			console.log("eachArticle:", eachArticle);
+
+			console.log(eachArticle);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
+};
+
+fetchData();
