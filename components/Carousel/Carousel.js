d31aaa17cc30b2bc93f3ed77fb90bd46224236dd
jtwray <tuckerwray84@gmail.com>
2019-08-18 23:10:10 -0400

    Creates a carouselFactory() to append carousel to the dom. Adjust styles of carouselContainer to a min/max width of 300/500px respectively, width styled at 100% between min-max for a smooth transition

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -7,6 +7,69 @@
     6. Have fun!
 */
 
+const carouselIndex = [
+	"C:/MAMP/htdocs/LambdaSchooL/Sprint-Challenge-Applied-Javascript/assets/carousel/computer.jpeg",
+	"C:/MAMP/htdocs/LambdaSchooL/Sprint-Challenge-Applied-Javascript/assets/carousel/mountains.jpeg",
+	"C:/MAMP/htdocs/LambdaSchooL/Sprint-Challenge-Applied-Javascript/assets/carousel/trees.jpeg",
+	"C:/MAMP/htdocs/LambdaSchooL/Sprint-Challenge-Applied-Javascript/assets/carousel/turntable.jpeg",
+];
+const createCarousel = () => {
+	const carouselDiv = document.createElement("div");
+	const leftBtn = document.createElement("div");
+  let carouselImage=document.createElement( "img" );
+  carouselImage.style.display="inherit";
+  carouselImage.style.minWidth="100%";
+	const rightBtn = document.createElement("div");
+	carouselDiv.classList.add("carousel");
+	carouselDiv.style.position = "inherit";
+	carouselDiv.style.height = "inherit";
+	carouselDiv.style.overflow = "inherit";
+	leftBtn.classList.add("left-button");
+  rightBtn.classList.add( "right-button" );
+  rightBtn.style.top="inherit";
+  rightBtn.style.transform="inherit";
+  rightBtn.style.right="0px";
+  leftBtn.style.right="0px";
+  leftBtn.style.top="inherit";
+  leftBtn.style.transform="inherit";
+	leftBtn.textContent = "<";
+	rightBtn.textContent = ">";
+	carouselDiv.appendChild(carouselImage);
+	carouselDiv.appendChild(leftBtn);
+	carouselDiv.appendChild(rightBtn);
+	const carousel = document.querySelector("div.carousel-container");
+  carousel.appendChild( carouselDiv );
+  carousel.style.width="100%";
+  carousel.style.minWidth="300px";
+  carousel.style.maxWidth="500px";
+  carousel.style.display="flex";
+
+	console.log("carouselIndex:", carouselIndex);
+
+	const parseCarouselImgs = () => {
+		let i = 0;
+		carouselImage.src = carouselIndex[i];
+		rightBtn.addEventListener("click", (event) => {
+			carouselIndex[i != 3 ? (i += 1) : (i = 0)];
+
+			// i==-1? ( i=3 ):( i=4? ( i=0 ):i=1|2|3? i:i );
+			carouselImage.src = carouselIndex[i];
+			console.log(carouselImage);
+			console.log(i);
+		});
+
+		leftBtn.addEventListener("click", (event) => {
+			// i == -1 ? (i = 3) : (i = 4 ? (i = 0) : i=1|2|3);
+
+			carouselImage.src = carouselIndex[i != 0 ? (i -= 1) : (i = 3)];
+			console.log(carouselImage);
+			console.log(i);
+		});
+	};
+	parseCarouselImgs();
+	return carouselDiv;
+};
+createCarousel();
 /* HTML:
   <div class="carousel">
     <div class="left-button"> < </div>
